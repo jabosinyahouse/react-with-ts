ff06cb3bdd1a2ffc3118ba6629da432952c3f203
jabeawsakul <s6230613036@phuket.psu.ac.th>
2022-04-09 10:46:26 +0700

    handling state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface IState {
+  people: {
+    name: string;
+    age: number;
+    number: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  // const [number, setNumber] = useState<number>(5);
+  const [people, setPeople] = useState<IState["people"]>();
+
+  // const changeNumber = () => {
+  //   let result = number + 1;
+  //   setNumber(result);
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
+      {/* <button onClick={() => changeNumber()}>Clckic meh</button> */}
+      {/* {number} */}
     </div>
   );
 }
